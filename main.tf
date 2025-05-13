34b4916d1da54c4fb9676d1ba74fd122a5e3c091
anil-goud <106153200+anil-goud@users.noreply.github.com>
2025-05-14 00:27:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-aws "aws_ec2intance"
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0dd574ef87b79ac6c"  # Replace with a valid AMI ID
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
