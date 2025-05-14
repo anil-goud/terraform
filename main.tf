5efb19f9ae9695951afa6c34efc3d28c56119e05
anil-goud <106153200+anil-goud@users.noreply.github.com>
2025-05-14 14:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0dd574ef87b79ac6c"  # Replace with a valid AMI ID
+  ami           = "ami-"  # Replace with a valid AMI ID
   instance_type = "t3.micro"
 
   tags = {
